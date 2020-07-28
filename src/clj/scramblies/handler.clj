351f95571edb8c69498b962b4ce953f915d4a5d7
Orlando Mendoza <orlandomr27@gmail.com>
2020-07-27 22:04:11 -0600

    removing flags

--- src/clj/scramblies/handler.clj
+++ src/clj/scramblies/handler.clj
@@ -10,8 +10,6 @@
 (defn handle-scramble [req]
   (let [s1 (str (get-in req [:query-params "s1"]))
         s2 (str (get-in req [:query-params "s2"]))
-        _ (println "s1: " s1)
-        _ (println "s2: " s2)
         result (scramble? s1 s2)]
     {:headers {"Content-type" "application/json"}
      :status 200
